9fb1efe4aef786b6c2e237e1f0df6cb2038b19d5
kylerdanielster <kylerdanielster@gmail.com>
2018-07-20 17:09:11 -0500

    appending card instead of rendering whole list

--- app/controllers/collection_cards_controller.rb
+++ app/controllers/collection_cards_controller.rb
@@ -3,7 +3,7 @@ class CollectionCardsController < ApplicationController
 
   def update
     @collection = Collection.find_by(id: params[:collection_id])
-    card = Card.find_by(id: params[:id])
-    @collection.cards << card
+    @card = Card.find_by(id: params[:id])
+    @collection.cards << @card
   end
 end
