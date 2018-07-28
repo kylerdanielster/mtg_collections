55e718b279f4cb68a4dad1fabbf08c4af8b016f3
kylerdanielster <kylerdanielster@gmail.com>
2018-07-27 23:29:37 -0500

    removed commented out code

--- app/controllers/card_search_controller.rb
+++ app/controllers/card_search_controller.rb
@@ -1,7 +1,6 @@
 class CardSearchController < ApplicationController
   before_action :get_current_collection, only: :index
   respond_to :js, only: :index
-  skip_before_action :verify_authenticity_token
 
   def index
       if params[:name]
@@ -18,11 +17,3 @@ class CardSearchController < ApplicationController
       @collection = Collection.find_by(id: params[:collection_id])
     end
 end
-
-
-
-# local
-# card_search?utf8=%E2%9C%93&name=Terminate&collection_id=55&commit=Search
-
-# prd
-# card_search?utf8=%E2%9C%93&name=Brainstorm&collection_id=1&commit=Search
